991570dd2de77f69699dfbf863ce792eed87fff8
Karina Ritchie <kritchie410@gmail.com>
2022-10-17 14:05:33 -0600

    feat: add post routes

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tenetur, temporibus ex atque officia quae vel, deserunt ipsa velit, unde corporis sit quia. Nesciunt repellat perspiciatis eius placeat et vel?
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tenetur, temporibus ex atque officia quae vel, deserunt ipsa velit, unde corporis sit quia. Nesciunt repellat perspiciatis eius placeat et vel?
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tenetur, temporibus ex atque officia quae vel, deserunt ipsa velit, unde corporis sit quia. Nesciunt repellat perspiciatis eius placeat et vel?
+        </p>
+    </article>
+</body>
