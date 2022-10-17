e109a9f4e268aa5d35c1e929dcba759fa795102d
Karina Ritchie <kritchie410@gmail.com>
2022-10-17 14:54:43 -0600

    feat: use caching for expensive operations

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,12 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if (! file_exists($path)) {
+    if (! file_exists($path =  __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
     }
 
-    cache()->remember("posts.{$slug}", 5, function () {
+    cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
 
-    });
-
-    $post = file_get_contents($path);
-
-    return view('post', [
-        'post' => $post
-    ]);
+    return view('post', ['post' => $post]);
 })->where('post', '[A-z_\-]+');
