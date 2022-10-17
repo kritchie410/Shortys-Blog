dec6d5e2bdb57d1c6082523d6199dfa8ee66cb88
Karina Ritchie <kritchie410@gmail.com>
2022-10-17 14:26:51 -0600

    feat: replace routes from previous merge

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if (! file_exists($path)) {
+        return redirect('/');
+    }
+
+    cache()->remember("posts.{$slug}", 5, function () {
+
+    });
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+');
