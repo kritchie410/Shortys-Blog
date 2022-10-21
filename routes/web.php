2b9b2cccae44ddf09e25f6679230c631dfc63a00
Karina Ritchie <kritchie410@gmail.com>
2022-10-21 12:51:00 -0600

    feat: connect new category model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
+use Illuminate\Database\Schema\PostgresSchemaState;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    // ddd($posts);
     return view('posts', [
         'posts' => Post::all()
     ]);
@@ -24,5 +25,10 @@ Route::get('/', function () {
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
